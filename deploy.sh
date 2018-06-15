aee83457b7273e40f3880b670c11f09cf226e373
Christian Treppo <christian@treppo.org>
2018-06-15 17:43:38 +0200

    Simplify deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
-echo $HEROKU_API_KEY | docker login --username _ --password-stdin registry.heroku.com
-docker tag application registry.heroku.com/yorck-ratings/web
-docker push registry.heroku.com/yorck-ratings/web
+heroku container:login
+heroku container:push web
 heroku container:release web --app yorck-ratings
