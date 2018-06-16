ef110abc776d57e1bd058129084b8a2303c4af8b
Christian Treppo <christian@treppo.org>
2018-06-16 15:08:03 +0200

    Move build and release instructions to scripts

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
-set -eux
+set -eu
 
-docker build --tag application --rm .
+docker build --tag registry.heroku.com/yorck-ratings/web --rm .
+echo ${HEROKU_API_KEY} | docker login --username _ --password-stdin registry.heroku.com
+docker push registry.heroku.com/yorck-ratings/web
---
+++ release.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+set -eu
+
+docker run --rm -e HEROKU_API_KEY=${HEROKU_API_KEY} dickeyxxx/heroku-cli:v7.0.98 heroku container:release web --app yorck-ratings
