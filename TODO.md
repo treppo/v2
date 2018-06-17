46938039e6970c302e57cfda9ef5ba5c8c3c1dae
Christian Treppo <christian@treppo.org>
2018-06-17 16:53:57 +0200

    Cleanup rated movie

--- TODO.md
+++ TODO.md
@@ -1,8 +1,7 @@
-* add deployment step to pipeline
 * rename yorck to cinema
 * infos should be called info
-* change css for highlighting
 * use clojure idiomatic regex in yorck
 * handle connection errors gracefully
+* try out vertx
 * add logging
 * add http level caching
--- src/yorck_ratings/rated_movie.clj
+++ src/yorck_ratings/rated_movie.clj
@@ -6,15 +6,6 @@
    :imdb-info   imdb-info
    :imdb-rating imdb-rating})
 
-(defn has-imdb-info? [rated-movie]
-  (:imdb-info rated-movie))
-
-(defn has-imdb-rating? [rated-movie]
-  (:imdb-rating rated-movie))
-
-(defn no-imdb-rating? [rated-movie]
-  (nil? (:imdb-rating rated-movie)))
-
 (defn from-yorck-info [[title url]]
   (make {:yorck-info [title url]}))
 
@@ -24,6 +15,7 @@
 (defn with-imdb-rating [rated-movie [rating rating-count]]
   (merge rated-movie {:imdb-rating [rating rating-count]}))
 
+
 (defn rating [rated-movie]
   (let [[rating count] (:imdb-rating rated-movie)]
     rating))
@@ -31,30 +23,6 @@
 (defn rating-count [rated-movie]
   (let [[rating count] (:imdb-rating rated-movie)]
     count))
-
-(def rating-threshold 7)
-(def count-threshold 1000)
-
-(defn count-above-threshold [rated-movie]
-  (>= (rating-count rated-movie) count-threshold))
-
-(defn count-below-threshold [rated-movie]
-  (< (rating-count rated-movie) count-threshold))
-
-(defn rating-above-threshold [rated-movie]
-  (>= (rating rated-movie) rating-threshold))
-
-(defn rating-below-threshold [rated-movie]
-  (< (rating rated-movie) rating-threshold))
-
-(defn hot? [rated-movie]
-  (and (has-imdb-rating? rated-movie) (rating-above-threshold rated-movie) (count-above-threshold rated-movie)))
-
-(defn considerable? [rated-movie]
-  (or
-    (no-imdb-rating? rated-movie)
-    (and (rating-above-threshold rated-movie)
-         (count-below-threshold rated-movie))))
 
 (defn yorck-title [rated-movie]
   (let [[title url] (:yorck-info rated-movie)]
@@ -72,7 +40,42 @@
   (let [[title url] (:imdb-info rated-movie)]
     url))
 
-(defn by-rating [a b]
+
+(defn has-imdb-info? [rated-movie]
+  (:imdb-info rated-movie))
+
+(defn has-imdb-rating? [rated-movie]
+  (:imdb-rating rated-movie))
+
+(defn no-imdb-rating? [rated-movie]
+  (not (has-imdb-rating? rated-movie)))
+
+
+(def rating-threshold 7)
+(def count-threshold 1000)
+
+(defn- count-above-threshold [rated-movie]
+  (>= (rating-count rated-movie) count-threshold))
+
+(defn- count-below-threshold [rated-movie]
+  (< (rating-count rated-movie) count-threshold))
+
+(defn- rating-above-threshold [rated-movie]
+  (>= (rating rated-movie) rating-threshold))
+
+(defn- rating-below-threshold [rated-movie]
+  (< (rating rated-movie) rating-threshold))
+
+(defn hot? [rated-movie]
+  (and (has-imdb-rating? rated-movie) (rating-above-threshold rated-movie) (count-above-threshold rated-movie)))
+
+(defn considerable? [rated-movie]
+  (or
+    (no-imdb-rating? rated-movie)
+    (and (rating-above-threshold rated-movie)
+         (count-below-threshold rated-movie))))
+
+(defn- by-rating [a b]
   (cond
     (and (no-imdb-rating? a) (no-imdb-rating? b)) 0
     (and (no-imdb-rating? a) (rating-above-threshold b)) -1
