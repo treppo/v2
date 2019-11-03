8f25f4e8578c1197450c153b24dd7eb1697b5600
Christian Treppo <christian@treppo.org>
2019-11-03 19:29:49 +0100

    Update dependencies

--- project.clj
+++ project.clj
@@ -3,20 +3,20 @@
   :url "https://yorck-ratings.treppo.org"
   :license {:name "Eclipse Public License"
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.10.0"]
-                 [clj-http "3.9.1"]
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [clj-http "3.10.0"]
                  [ring/ring-core "1.7.1"]
                  [ring/ring-jetty-adapter "1.7.1"]
                  [hickory "0.7.1"]
                  [hiccup "1.0.5"]
-                 [org.clojure/core.async "0.4.490"]]
+                 [org.clojure/core.async "0.4.500"]]
   :local-repo ".m2"
   :aot [yorck-ratings.web]
   :main yorck-ratings.web
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}
              :dev     {:dependencies   [[clj-http-fake "1.0.3"]
-                                        [ring/ring-mock "0.3.2"]]
+                                        [ring/ring-mock "0.4.0"]]
                        :resource-paths ["test/resources"]
                        :plugins        [[lein-jlink "0.2.1"]
                                         [lein-ancient "0.6.15"]
